75c2efb916421ca4722050a7819a49c7d3abe113
gmreads <75183034+gmreads@users.noreply.github.com>
2021-04-13 21:08:01 +0530

    fix -Wall errors unused variables

--- CF-A/vanya-fence.cpp
+++ CF-A/vanya-fence.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int main(int argc, char * argv[]) {
+int main() {
     int num_friends, fence_height;
     int normal_width = 1;
     int bend_width = 2;
@@ -12,10 +12,10 @@ int main(int argc, char * argv[]) {
         int friend_height = 0;
 	std::cin>>friend_height;
         if(friend_height > fence_height){
-	    total_width += 2;
+	    total_width += bend_width;
 	}	
 	else{
-	    total_width+=1;
+	    total_width += normal_width;
 	}
     }
     std::cout<<total_width;
