d791ba9e274a4b2b07b741d93abf8e45c047d332
gmreads <75183034+gmreads@users.noreply.github.com>
2021-04-17 16:04:37 +0530

    Second problem  - learn if, else, char  - Code improvements: Use string/array for input instead of char

---
+++ CF-A/anton-danik.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+int main() {
+    int number_of_games;
+    char person_won;
+    int anton_wins = 0;
+    int danik_wins = 0;
+    std::cin>>number_of_games;
+    while (number_of_games-- > 0) {
+        std::cin>>person_won;
+        if (person_won == 'A') {
+            anton_wins++;
+        } else {
+            //Input string only has A,D
+            danik_wins ++;
+        }
+    }
+    if (anton_wins == danik_wins) {
+        std::cout<<"Friendship";
+    } else {
+        if (anton_wins > danik_wins) {
+            std::cout<<"Anton";
+        } else {
+            std::cout<<"Danik";
+        }
+    }
+
+    return 0;
+
+}
